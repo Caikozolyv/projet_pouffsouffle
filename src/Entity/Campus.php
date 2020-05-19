3b3eac740a0910528935481b563f5c10adbe0526
Emeline FE <emeline.feron@laposte.net>
2020-05-19 15:58:20 +0200

    Campus update

--- src/Entity/Campus.php
+++ src/Entity/Campus.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CampusRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +27,24 @@ class Campus
      * @ORM\Column(type="string", length=255)
      */
     private $nom;
+
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Sorties", mappedBy="campus")
+     */
+    private $listeSorties;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Participants", mappedBy="campus")
+     */
+    private $listeParticipants;
+
+    public function __construct()
+    {
+        $this->listeParticipants = new ArrayCollection();
+        $this->listeSorties = new ArrayCollection();
+    }
+
 
     public function getId(): ?int
     {
@@ -55,4 +74,39 @@ class Campus
 
         return $this;
     }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getListeSorties(): ArrayCollection
+    {
+        return $this->listeSorties;
+    }
+
+    /**
+     * @param ArrayCollection $listeSorties
+     */
+    public function setListeSorties(ArrayCollection $listeSorties): void
+    {
+        $this->listeSorties = $listeSorties;
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getListeParticipants(): ArrayCollection
+    {
+        return $this->listeParticipants;
+    }
+
+    /**
+     * @param ArrayCollection $listeParticipants
+     */
+    public function setListeParticipants(ArrayCollection $listeParticipants): void
+    {
+        $this->listeParticipants = $listeParticipants;
+    }
+
+
+
 }
