26b5a114999b1345b1242f8f450bfb88447904ee
Caikozolyv <valentinfarcy@gmail.com>
2020-05-20 15:06:45 +0200

    fixing lieuRepository bug

--- src/Entity/Lieu.php
+++ src/Entity/Lieu.php
@@ -6,7 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=LieuRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\LieuRepository")
  */
 class Lieu
 {
