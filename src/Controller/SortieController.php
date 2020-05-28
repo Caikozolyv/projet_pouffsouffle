7d30734a6a0c436ffc19dfa818b9d680b982d005
Caikozolyv <valentinfarcy@gmail.com>
2020-05-28 18:25:38 +0200

    dynamic dropdown ville / lieux to be continued... im in love with the coco

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -9,11 +9,14 @@ use App\Entity\Ville;
 use App\Form\LieuType;
 use App\Form\SearchSortieType;
 use App\Form\SortieType;
+use App\Repository\LieuRepository;
 use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use App\Repository\VilleRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -52,57 +55,91 @@ class SortieController extends AbstractController
     }
 
     /**
+     * @Route("/selectVille", name="sortie_select_ville", methods={"GET", "POST"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function selectVille(Request $request, LieuRepository $lr) {
+//        if($request->isXmlHttpRequest()) {
+//            if($request->get('idVille')) {
+                $idVille = $request->get('villeId');
+                $lieux = $lr->findLieuxByVilleId($idVille);
+                $tabLieux = array();
+//                $i = 0;
+//
+                foreach ($lieux as $lieu) {
+//                    $tabLieux[$i]['id'] = $lieu->getIdLieu();
+//                    $tabLieux[$i]['nom'] = $lieu->getNomLieu();
+//                    $i++;
+                    $tabLieux[] = array(
+                        "id" => $lieu->getIdLieu(),
+                        "name" => $lieu->getNomLieu()
+                    );
+                }
+                return new JsonResponse($tabLieux);
+//
+//                $response = new Response();
+//                $data = json_encode($tabLieux);
+//                $response->headers->set('Content-Type', 'application/json');
+//                $response->setContent($data);
+//
+//                return $response;
+//            }
+//        } else {
+//            return new Response('no ajax');
+//        }
+//        return new Response('no ajax');
+    }
+
+    /**
      * @Route("/new", name="sortie_new", methods={"GET","POST"})
      */
-    public function new(Request $request, VilleRepository $vr): Response
+    public function new(Request $request, VilleRepository $vr, LieuRepository $lr): Response
     {
         $sortie = new Sortie();
         $lieu = new Lieu();
         $campus = new Campus();
         $ville = new Ville();
 
-
-        $villes = $vr->findAll();
         $form = $this->createForm(SortieType::class, $sortie);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $sortie->setName($sortie->getName());
-            $sortie->setDateHeureDebut($sortie->getDateHeureDebut());
-            $sortie->setDateLimiteInscription($sortie->getDateLimiteInscription());
-            $sortie->setNbInscriptionMax($sortie->getNbInscriptionMax());
-            $sortie->setDuree($sortie->getDuree());
-            $sortie->setInfosSortie($sortie->getInfosSortie());
-
-            $campus->setNom($campus->getName());
-
-            $lieu->setNomLieu($lieu->getNomLieu());
-            $lieu->setRue($lieu->getNomLieu());
-            $lieu->setLatitude($lieu->getLatitude());
-            $lieu->setLongitude($lieu->getLongitude());
-
-            //Ville ne marche toujours pas, tralalalalala
-            $ville->setNomVille($ville->getNomVille());
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($sortie);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('sortie');
-
-            //La création ne marche pas :(
-
-
-
-        }
+//        if ($form->isSubmitted() && $form->isValid()) {
+//
+//            $sortie->setName($sortie->getName());
+//            $sortie->setDateHeureDebut($sortie->getDateHeureDebut());
+//            $sortie->setDateLimiteInscription($sortie->getDateLimiteInscription());
+//            $sortie->setNbInscriptionMax($sortie->getNbInscriptionMax());
+//            $sortie->setDuree($sortie->getDuree());
+//            $sortie->setInfosSortie($sortie->getInfosSortie());
+//
+//            $campus->setNom($campus->getName());
+//
+//            $lieu->setNomLieu($lieu->getNomLieu());
+//            $lieu->setRue($lieu->getNomLieu());
+//            $lieu->setLatitude($lieu->getLatitude());
+//            $lieu->setLongitude($lieu->getLongitude());
+//
+//            //Ville ne marche toujours pas, tralalalalala
+//            $ville->setNomVille($ville->getNomVille());
+//
+//            $entityManager = $this->getDoctrine()->getManager();
+//            $entityManager->persist($sortie);
+//            $entityManager->flush();
+//
+//            return $this->redirectToRoute('sortie');
+//
+//            //La création ne marche pas :(
+//
+//
+//
+//        }
 
         return $this->render('sortie/new.html.twig', [
 
             //Je ne sais pas où le mettre car ici il ne veut pas pourquoi??
             //'sortie' => $sortieRepository->findSortie(),
-            'villes' => $villes,
-            'sortie' => $sortie,
+//            'sortie' => $sortie,
             'form' => $form->createView(),
         ]);
     }
--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -6,6 +6,8 @@ use App\Entity\Sortie;
 use App\Entity\Lieu;
 use App\Entity\Campus;
 use App\Entity\Ville;
+use App\Repository\VilleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
@@ -13,10 +15,26 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SortieType extends AbstractType
 {
+    private $em;
+
+    /**
+     * The Type requires the EntityManager as argument in the constructor. It is autowired
+     * in Symfony 3.
+     *
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -32,42 +50,128 @@ class SortieType extends AbstractType
                 'label' => "Durée : "
             ])
             ->add('infosSortie', TextareaType::class, [
-                'label' =>"Description et infos : ",
+                'label' => "Description et infos : ",
                 'attr' => [
                     'class' => 'overview-txt'
                 ]
             ])
             ->add('campus', EntityType::class, [
+                'attr' => [
+                    'readonly' => true
+                ],
                 'class' => Campus::class,
                 'label' => "Campus",
-                'query_builder' => function(EntityRepository $repository) {
+                'query_builder' => function (EntityRepository $repository) {
                     return $repository->createQueryBuilder('c')->orderBy('c.nom', 'ASC');
                 }
             ])
-
             #La classe Ville ne veut TOUJOURS pas s'injecter dans le formulaire
             #Help, Au secours, A votre bon coeur pour aider!!!
 
-            #->add('ville', EntityType::class, [
-                #'class' =>Ville::class,
-                #'label' => "ville",
-                #'query_builder' => function(EntityRepository $repository) {
-                    #return $repository->createQueryBuilder('c')->orderBy('c.nomVille', 'ASC');
-                #}
-            #])
-
-
-
-            ->add('lieu', EntityType::class, [
-                'class' => Lieu::class,
-                'label' => "Lieu",
-                'query_builder' => function(EntityRepository $repository) {
-                    return $repository->createQueryBuilder('c')->orderBy('c.nomLieu', 'ASC');
-    }
+            ->add('ville', EntityType::class, [
+                'mapped' => false,
+                'class' => Ville::class,
+                'label' => "Ville",
+                'placeholder' => 'Choisissez une ville',
+                'query_builder' => function (EntityRepository $repository) {
+                    return $repository->createQueryBuilder('c')->orderBy('c.nomVille', 'ASC');
+                }
             ])
+//            ->add('lieu', EntityType::class, [
+//                        'class' => Lieu::class,
+//                        'label' => "Lieu",
+//                        'disabled' => true,
+//                        'query_builder' => function(EntityRepository $repository) {
+//                            return $repository->createQueryBuilder('c')->orderBy('c.nomLieu', 'ASC');
+//                        }
+//                    ])
+//            ->addEventListener(
+//                FormEvents::PRE_SET_DATA,
+//                function (FormEvent $event) {
+//                    $form = $event->getForm();
+//                    $data = $event->getData();
+//                    dump($data);
+//                    $ville = $data->getVille();
+//                    $lieux = null === $ville ? [] : $ville->getLieux();
+//
+//                    $form->add('lieu', EntityType::class, [
+//                       'class'=> Lieu::class,
+//                       'choices' => $lieux
+//                    ]);
+//                }
+//            );
+//        }
+            ->addEventListener(FormEvents::PRE_SET_DATA, array($this, 'onPreSetData'))
+            ->addEventListener(FormEvents::PRE_SUBMIT, array($this, 'onPreSubmit'));
+            }
 
-        ;
+    protected function addElements(FormInterface $form, Ville $ville = null)
+    {
+        dump($ville);
+        $lieux = array();
+
+        if ($ville) {
+            $lr = $this->em->getRepository('LieuRepository');
+            $lieux = $lr->createQueryBuilder('q')
+                ->andWhere("q.ville = :villeId")
+                ->setParameter("villeId", $ville->getId())
+                ->getQuery()
+                ->getResult();
+        }
+
+        $form->add('lieu', EntityType::class, [
+            'class' => Lieu::class,
+            'label' => "Lieu",
+            'attr' => [
+                'disabled' => true
+            ],
+            'query_builder' => function(EntityRepository $repository) {
+                return $repository->createQueryBuilder('c')->orderBy('c.nomLieu', 'ASC');
+            }
+        ]
+        );
     }
+
+    function onPreSubmit(FormEvent $event, VilleRepository $vr)
+    {
+        $form = $event->getForm();
+        $data = $event->getData();
+
+        // Search for selected City and convert it into an Entity
+        $ville = $vr->find($data['ville']);
+        dump($ville);
+
+        $this->addElements($form, $ville);
+    }
+
+    function onPreSetData(FormEvent $event)
+    {
+        $sortie = $event->getData();
+        $form = $event->getForm();
+        $ville = $form->get('ville')->getData() ? $form->get('ville')->getData() : null;
+        dump($ville);
+        $this->addElements($form, $ville);
+    }
+
+
+//            ->addEventListener(FormEvents::PRE_SET_DATA,
+//                function (FormEvent $event) {
+//                    $form = $event->getForm();
+////                    $data = $event->getData();
+//                    $ville = $form->get('ville')->getData();
+//                    dump($ville);
+//                    $lieux = null === $ville ? [] : $ville->getLieux();
+//
+//                    $form->add('lieu', EntityType::class, [
+//                        'class' => Lieu::class,
+//                        'label' => "Lieu",
+//                        'choices' => $lieux
+////                        'query_builder' => function(EntityRepository $repository) {
+////                            return $repository->createQueryBuilder('c')->orderBy('c.nomLieu', 'ASC');
+////                        }
+//                    ]);
+//
+//                })
 
     public function configureOptions(OptionsResolver $resolver)
     {
