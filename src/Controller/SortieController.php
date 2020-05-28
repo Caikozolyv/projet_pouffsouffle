f1661ab64b7ff20c7d46cdf2b8270b1411e2922c
Caikozolyv <valentinfarcy@gmail.com>
2020-05-28 23:24:49 +0200

    add une sortie ça fonctionne ... enfin ! Have a good night everybody <3

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,21 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\Campus;
 use App\Entity\Etat;
 use App\Entity\FindSortie;
-use App\Entity\Lieu;
 use App\Entity\Participant;
 use App\Entity\Sortie;
-use App\Entity\Ville;
-use App\Form\LieuType;
 use App\Form\SearchSortieType;
 use App\Form\SortieType;
 use App\Repository\LieuRepository;
 use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
-use App\Repository\VilleRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -24,8 +18,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\Date;
-use Symfony\Component\Validator\Constraints\DateTime;
-use Symfony\Component\Validator\Constraints\Time;
 
 /**
  * @Route("/sortie")
@@ -108,64 +100,49 @@ class SortieController extends AbstractController
     /**
      * @Route("/new", name="sortie_new", methods={"GET","POST"})
      */
-    public function new(Request $request, VilleRepository $vr, LieuRepository $lr): Response
+    public function new(Request $request): Response
     {
         $sortie = new Sortie();
-        $lieu = new Lieu();
-        $campus = new Campus();
-        $ville = new Ville();
 
         //Ligne rajout Victor pour Etat
         $etatController = new EtatController();
 
-
-        $villes = $vr->findAll();
         $form = $this->createForm(SortieType::class, $sortie);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
+            dump($sortie);
             $sortie->setName($sortie->getName());
             $sortie->setDateHeureDebut($sortie->getDateHeureDebut());
             $sortie->setDateLimiteInscription($sortie->getDateLimiteInscription());
             $sortie->setNbInscriptionMax($sortie->getNbInscriptionMax());
             $sortie->setDuree($sortie->getDuree());
             $sortie->setInfosSortie($sortie->getInfosSortie());
-
-            $campus->setNom($campus->getName());
-
-            $lieu->setNomLieu($lieu->getNomLieu());
-            $lieu->setRue($lieu->getNomLieu());
-            $lieu->setLatitude($lieu->getLatitude());
-            $lieu->setLongitude($lieu->getLongitude());
+            $sortie->setCampus($sortie->getCampus());
+            $sortie->setLieu($sortie->getLieu());
 
             //Ville ne marche toujours pas, tralalalalala
-            $ville->setNomVille($ville->getNomVille());
+//            $ville->setNomVille($ville->getNomVille());
 
             //Lignes ajout Victor pour Etat
-            $repoEtats = $etatController->getDoctrine()->getManager()->getRepository();
-            $ouverte = $repoEtats->find(2);
-            $sortie->setEtat($ouverte);
-            $ouverte = $repoEtats->find(2);
-            $sortie->setEtat($ouverte);
+//            $repoEtats = $etatController->getDoctrine()->getManager()->getRepository();
+//            $ouverte = $repoEtats->find(2);
+//            $sortie->setEtat($ouverte);
+//            $ouverte = $repoEtats->find(2);
+
+            //je l'ai mis en dur en attendant parce que ce qu'il y a au dessus ne fonctionne pas
+            //bonne nuit !
+            $sortie->setEtat('ouverte');
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($sortie);
             $entityManager->flush();
 
-            return $this->redirectToRoute('sortie');
-
-            //La création ne marche pas :(
-
-
+            return $this->redirectToRoute('sortie_index');
 
         }
 
         return $this->render('sortie/new.html.twig', [
-
-            //Je ne sais pas où le mettre car ici il ne veut pas pourquoi??
-            //'sortie' => $sortieRepository->findSortie(),
-            'villes' => $villes,
             'sortie' => $sortie,
             'form' => $form->createView(),
         ]);
